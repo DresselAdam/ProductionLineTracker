5ecd66c105e526cb91cab926712462cb2933eaaa
Adam Dressel <dresseladam3@users.noreply.github.com>
2019-09-24 13:12:36 -0400

    Added UI for tabs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import java.sql.PreparedStatement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField productNameTxt;
+    @FXML
+    private Button productLineBtn;
+    @FXML
+    private Label manufacturerLbl;
+
+
+
+    public void addProd(ActionEvent clicked) {
+        System.out.println("This is the product line");
+
+    }
+
+    public void recProd(ActionEvent clicked) {
+        System.out.println("Record Production");
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,54 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  private Connection conn;
+  private PreparedStatement psmt;
+  private ResultSet rset;
+  final String JDBC_DRIVER = "org.h2.Driver";
+  //final String DB_path =
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
 
 
-    public static void main(String[] args) {
-        launch(args);
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+      primaryStage.setTitle("Hello World");
+      primaryStage.setScene(new Scene(root, 600, 550));
+      primaryStage.show();
     }
-}
+   catch(Exception e){
+      e.printStackTrace();
+    }
+
+    // end try catch
+  }// end start method
+
+  public void initializeDatabase() {
+
+    String query = "";
+
+    try {
+
+
+    }catch (Exception db){
+
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}// class main
