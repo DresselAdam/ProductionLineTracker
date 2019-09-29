eb98bd5585e8f825b8c58d6f5ae8294a0fce9d12
Adam Dressel <dresseladam3@users.noreply.github.com>
2019-09-28 21:35:54 -0400

    Sprint 1 features and functionality

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,29 +1,143 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+/**
+ * Controller class drives the program connecting the UI with general functionality.
+ *
+ * @author Adam Dressel
+ */
 public class Controller {
-    @FXML
-    private TextField productNameTxt;
-    @FXML
-    private Button productLineBtn;
-    @FXML
-    private Label manufacturerLbl;
 
+  @FXML
+  private TextField productNameTxt;
+  @FXML
+  private TextField manufacturerTxt;
+  @FXML
+  private Button productLineBtn;
+  @FXML
+  private ChoiceBox<String> itemTypeChb;
+  private final String[] itemTypes = {"Audio", "Visual", "AudioMobile", "VisualMobile"};
+  @FXML
+  // Combo box for the produce tab.
+  private ComboBox<Integer> chQntCb;
 
+  private Connection conn;
 
-    public void addProd(ActionEvent clicked) {
-        System.out.println("This is the product line");
+  /**
+   * initialize method is the driver for the program. This method is called in main once the UI is
+   * generated. All other methods are called from the initialize method.
+   */
+  public void initialize() {
+    setItemTypeChb(itemTypes, itemTypeChb);
 
+    initializeDatabase();
+
+    productLineBtn.setOnAction(this::addProd);
+
+    setChQntCb();
+
+    //showProducts();
+
+  }
+
+  /**
+   * initializeDatabase sets up the connection to the H2 product database. Since this connection is
+   * made at the beginning, multiple  queries can be made without creating another connection.
+   */
+  private void initializeDatabase() {
+    String path = "jdbc:h2:./res/ProdDB";
+    final String jdbcDriver = "org.h2.Driver";
+
+    try {
+      Class.forName(jdbcDriver);
+
+      conn = DriverManager.getConnection(path);
+      System.out.println("Connection Successful");
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public void recProd(ActionEvent clicked) {
-        System.out.println("Record Production");
+  /**
+   * addProd method is used to insert products into the database. This is done through the UI by
+   * extracting the values for the textfields and choice box and using them in a prepared
+   * statement.
+   *
+   * @param addProdEvent Signals a button event for the setOnAction method
+   */
+  // Triggers warning in inspect code but I need parameter to trigger event.
+  public void addProd(ActionEvent addProdEvent) {
+
+    System.out.println("Inserting records into the table...");
+    String prodName = productNameTxt.getText();
+    String manufacturer = manufacturerTxt.getText();
+    String itemType = itemTypeChb.getValue();
+    try {
+      PreparedStatement pstmtUpdate;
+
+      String query = "INSERT INTO PRODUCT(NAME,TYPE,MANUFACTURER)" + " VALUES (?,?,?)";
+
+      // Bug says the statement may not be closed in the event of an error.
+      pstmtUpdate = conn.prepareStatement(query);
+
+      pstmtUpdate.setString(1, prodName);
+      pstmtUpdate.setString(2, manufacturer);
+      pstmtUpdate.setString(3, itemType);
+      pstmtUpdate.executeUpdate();
+
+      pstmtUpdate.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
+  }
+
+  /*
+    private void showProducts() {
+      String query = "SELECT NAME, TYPE, MANUFACTURER FROM PRODUCT";
+      try {
+        rset = pstmtUpdate.executeQuery(query);
+        while (rset.next()) {
+          addProdTbv.getItems().add(rset.getString("name"));
+        }
+      } catch (Exception he) {
+        he.printStackTrace();
+      }
+    }
+  */
+
+  /**
+   * Sets the item types for the choice box in a for each loop.
+   *
+   * @param types Takes an array of strings to assign to the choice box.
+   * @param cb The choice box is passed so that it can use the data in the array.
+   */
+  private void setItemTypeChb(String[] types, ChoiceBox<String> cb) {
+    for (String type : types) {
+      cb.getItems().add(type);
+    }
+  }
+
+  /**
+   * sets the choices for the combo box in the produce tab. Also calls two methods for additional
+   * functionality.
+   */
+  private void setChQntCb() {
+    for (int choices = 1; choices <= 10; choices++) {
+      chQntCb.getItems().add(choices);
+    }
+    chQntCb.setEditable(true);
+    chQntCb.getSelectionModel().selectFirst();
+  }
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,54 +1,36 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Control;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+// Sources: https://docs.oracle.com https://www.tutorialspoint.com/javafx/
+// https://www.tutorialspoint.com/jdbc http://tutorials.jenkov.com/javafx/
 
+/**
+ * Main initializes UI and starts the Controller class.
+ *
+ * @author Adam Dressel
+ */
 public class Main extends Application {
 
-  private Connection conn;
-  private PreparedStatement psmt;
-  private ResultSet rset;
-  final String JDBC_DRIVER = "org.h2.Driver";
-  //final String DB_path =
-
   @Override
-  public void start(Stage primaryStage) throws Exception {
-
+  public void start(Stage primaryStage) {
 
     try {
       Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
       primaryStage.setTitle("Hello World");
       primaryStage.setScene(new Scene(root, 600, 550));
+      root.getStylesheets().add("sample/production_style.css");
       primaryStage.show();
-    }
-   catch(Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
-    }
+    } // end try catch
+  } // end start method
 
-    // end try catch
-  }// end start method
-
-  public void initializeDatabase() {
-
-    String query = "";
-
-    try {
-
-
-    }catch (Exception db){
-
-    }
-  }
 
   public static void main(String[] args) {
     launch(args);
   }
-}// class main
+} // class main
